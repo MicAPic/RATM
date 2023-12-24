5376acbe8302cc18b771a5b5522b16bd3900c39e
Michael Pavlov <73939030+MicAPic@users.noreply.github.com>
2023-12-24 19:34:03 +0300

    Spawn agent on roads

--- Assets/Scripts/AI/CarAgent.cs
+++ Assets/Scripts/AI/CarAgent.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace AI
 {
@@ -9,12 +12,36 @@ namespace AI
     public class CarAgent : Agent
     {
         public bool trainingMode;
+
+        [FormerlySerializedAs("minimalZ")]
+        [Header("Training Settings")]
+        [Tooltip("If the agent is below this mark, he's considered to be out of bounds")]
+        [SerializeField]
+        private float minimalY;
+        [Tooltip("If the agent is within this threshold, he is considered to have visited a bug location")]
+        [SerializeField]
+        private float threshold = 1.0f;
+        [SerializeField]
+        private float minimalFps = 15.0f;
+
+        [Header("Spawn Settings")]
+        [SerializeField]
+        private LayerMask roadMask;
+        [SerializeField]
+        private Collider stageAreaCollider;
+
+        [Space]
+        [SerializeField]
+        private Transform sphereTransform;
+        
         private bool _frozen = false;
         private CarController _carController;
-
+        private Vector3 _sphereStartingPosition;
+        private List<Vector3> _visitedBugLocations = new List<Vector3>();
         private void Awake()
         {
             _carController = GetComponent<CarController>();
+            _sphereStartingPosition = sphereTransform.localPosition;
         }
     
         // Start is called before the first frame update
@@ -24,10 +51,23 @@ namespace AI
         // }
 
         // Update is called once per frame
-        // void Update()
-        // {
-        //     
-        // }
+        void Update()
+        {
+            CheckForOutOfBounds();
+            CheckFps();
+        }
+
+        /// <summary>
+        /// Reset the agent when an episode begins
+        /// </summary>
+        public override void OnEpisodeBegin()
+        {
+            if (trainingMode)
+            {
+                _visitedBugLocations = new List<Vector3>();
+                MoveToSafeRandomPosition();
+            }
+        }
 
         /// <summary>
         /// Index 0: Braking & Acceleration values (from -1 to 1)<br></br>
@@ -51,7 +91,80 @@ namespace AI
 
         public override void CollectObservations(VectorSensor sensor)
         {
-            sensor.AddObservation(transform.position);
+            sensor.AddObservation(transform.localPosition);
+        }
+
+        private void CheckForOutOfBounds()
+        {
+            var position = transform.localPosition;
+            if (position.y > minimalY) return;
+            
+            if (!HasVisitedBugLocation(position))
+            {
+                AddReward(1.0f);
+                AddBugLocation(position, "out of bounds");
+                MoveToSafeRandomPosition();
+            }
+        }
+
+        private void CheckFps()
+        {
+            var fps = 1 / Time.unscaledDeltaTime;
+            if (fps > minimalFps) return;
+
+            if (!HasVisitedBugLocation(transform.localPosition))
+            {
+                AddReward(.1f);
+                AddBugLocation(transform.localPosition, "fps");
+            }
+        }
+
+        private void AddBugLocation(Vector3 currentPosition, string type)
+        {
+            _visitedBugLocations.Add(new Vector3(currentPosition.x, 0.0f, currentPosition.z));
+            Debug.LogWarning($"Found a(n) {type} bug at: {currentPosition}");
+            var color = type switch
+            {
+                "fps" => Color.red,
+                "out of bounds" => Color.cyan,
+                _ => Color.white
+            };
+            var upward = transform.TransformDirection(Vector3.up) * 100;
+            Debug.DrawRay(transform.position, upward, color, float.PositiveInfinity);
+        }
+
+        private bool HasVisitedBugLocation(Vector3 currentPosition)
+        {
+            currentPosition = new Vector3(currentPosition.x, 0.0f, currentPosition.z);
+            return _visitedBugLocations.Any(location => Vector3.Distance(location, currentPosition) < threshold);
+        }
+
+        private void MoveToSafeRandomPosition()
+        {
+            var safePositionFound = false;
+            var attemptsRemaining = 100; // Prevent an infinite loop
+            var potentialPosition = Vector3.zero;
+
+            while (!safePositionFound && attemptsRemaining > 0)
+            {
+                attemptsRemaining--;
+                
+                var bounds = stageAreaCollider.bounds;
+                var offsetX = Random.Range(-bounds.extents.x, bounds.extents.x);
+                var offsetY = Random.Range(-bounds.extents.y, bounds.extents.y);
+                var offsetZ = Random.Range(-bounds.extents.z, bounds.extents.z);
+                
+                potentialPosition = bounds.center + new Vector3(offsetX, offsetY, offsetZ);
+                
+                if (Physics.Raycast(potentialPosition, Vector3.down, 100.0f, roadMask))
+                {
+                    var colliders = Physics.OverlapSphere(potentialPosition, 5f);
+                    safePositionFound = colliders.Length == 0;
+                }
+            }
+            
+            sphereTransform.position  = potentialPosition;
+            sphereTransform.localRotation = Quaternion.identity;
         }
     }
 }
